1a8f11c565f9a04fd02accbc048c7dcb98cf68a1
Mincky <54250862+mickers13@users.noreply.github.com>
2020-06-08 11:14:58 +0200

    Write text to file, text only

--- Src/Main.py
+++ Src/Main.py
@@ -2,16 +2,19 @@
 A test URL writing program, going t
 """
 from mitmproxy import http
+from mitmproxy.net.http.http1.assemble import assemble_request
+from bs4 import BeautifulSoup
+from mitmproxy import ctx, http
+
+from mitmproxy.net.http.http1.assemble import _assemble_response_headers, assemble_response
 
 
 def request(flow: http.HTTPFlow) -> None:
-    # pretty_url takes the "Host" header of the request into account, which
-    # is useful in transparent mode where we usually only have the IP otherwise.
+    # alles wat in de persoon zijn requist zit kan je hier vragen.
 
-    with open("C:/Users/Orang/PycharmProjects/Ipass/Src/textfile.txt","a")as test:
+    with open("C:/Users/Orang/PycharmProjects/Ipass/Src/textfile.txt", "a")as test:
         url = flow.request.pretty_url + "\n"
         test.write(url)
-
 
     if flow.request.pretty_url == "http://example.com/path":
         flow.response = http.HTTPResponse.make(
@@ -19,3 +22,12 @@ def request(flow: http.HTTPFlow) -> None:
             b"Hello World",  # (optional) content
             {"Content-Type": "text/html"}  # (optional) headers
         )
+
+
+def response(flow: http.HTTPFlow):
+    # alles wat in de response zit kan je hier vragen.
+    with open("C:/Users/Orang/PycharmProjects/Ipass/Src/test2html.txt", "a+")as test:
+        data = assemble_request(flow.request).decode('utf-8')
+        httpdata = BeautifulSoup(flow.response.content, "html.parser")
+        httptext = httpdata.get_text()
+        test.write(httptext)
